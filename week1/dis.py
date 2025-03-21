becd1aeeb4e3cf8fc3a170b35eb6d9d939add940
AkshayKalashikam <akshaykalashikam@gmail.com>
2025-03-21 15:49:51 +0530

    Update dis.py

--- week1/dis.py
+++ week1/dis.py
@@ -2,5 +2,5 @@ a1=int(input("enter a1 value:"))
 a2=int(input("enter a2 value:"))
 b1=int(input("enter b1 value:"))
 b2=int(input("enter b2 value:")) 
-dis=((a2-a1)**(b2-b1))**(1/2)
-print("distance between two points is;",dis)
+dis=((a2-a1)**2+(b2-b1)**2)**0.5
+print("distance between two points is;",round(dis,2))
