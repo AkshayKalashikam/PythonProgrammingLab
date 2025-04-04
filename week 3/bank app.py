b3bb2fe6fe2e1f0d40c86c3760f273ad21639ad2
AkshayKalashikam <akshaykalashikam@gmail.com>
2025-04-04 15:49:09 +0530

    Create bank app2.py

---
+++ week 3/bank app.py
@@ -0,0 +1,20 @@
+def credit (bal,value):
+    bal = bal + value
+    return bal
+    
+def debit (bal,value):  
+    bal = value - bal
+    return bal
+    
+def show_bal(bal):
+    print (bal)
+    
+bal = int(input("enter the amount:"))
+amount = int(input("enter the amount to be credited or debited"))
+sys = int(input("pres1 to credit \n press2 to debit"))
+if(sys==1):
+   balance = credit(amount,bal)
+   show_bal(bal)
+if(sys==2):
+   bal = debit(amount,bal)
+   show_bal(bal)
